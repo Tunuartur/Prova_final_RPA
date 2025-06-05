cd60a380a9826e0fd3ddea42ab39d56c78898919
Artur Tunu <tunuartur@gmail.com>
2025-06-05 19:37:34 -0300

    projeto finalizado

--- projeto_rpa.py
+++ projeto_rpa.py
@@ -69,3 +69,37 @@ conn.commit()
 conn.close()
 
 print("Dados processados e inseridos na tabela dados_processados com sucesso!")
+
+import sqlite3
+import smtplib
+from email.message import EmailMessage
+import getpass
+
+conn = sqlite3.connect('projeto_rpa.db')
+cursor = conn.cursor()
+cursor.execute("SELECT nome, genero, localizacao FROM dados_processados")
+dados = cursor.fetchall()
+conn.close()
+
+resumo = "Resumo dos personagens processados:\n\n"
+for nome, genero, local in dados:
+    resumo += f"Nome: {nome}\nGênero: {genero}\nLocalização: {local}\n\n"
+
+print("=== Configuração do e-mail ===")
+EMAIL_ORIGEM = input("Seu e-mail Gmail: ")
+EMAIL_SENHA = input("Senha de app do Gmail (senha gerada pelo email!): ")
+EMAIL_DESTINO = input("E-mail de destino: ")
+
+msg = EmailMessage()
+msg['Subject'] = 'Relatório Final RPA - Personagens de Rick and Morty'
+msg['From'] = EMAIL_ORIGEM
+msg['To'] = EMAIL_DESTINO
+msg.set_content(resumo)
+
+try:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ORIGEM, EMAIL_SENHA)
+        smtp.send_message(msg)
+    print("✅ E-mail enviado com sucesso!")
+except Exception as e:
+    print("❌ Falha ao enviar e-mail:", e)
